0d23c84a038deab4c3d1201ed17fed3af550fa1a
Suhas3110 <suhasd3110@gmail.com>
2025-03-18 12:44:07 +0530

    Add project files.

---
+++ Library_mgt_system/Controllers/AdministrationController.cs
@@ -0,0 +1,45 @@
+﻿using Library_mgt_system.Models;
+using Library_mgt_system.Models.validation_classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library_mgt_system.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private Library_mgt_systemEntities dbo = new Library_mgt_systemEntities();
+        // GET: Administration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(tblloginModel lg)
+        {
+            if (ModelState.IsValid)
+            {
+                var log = dbo.tbl_login.FirstOrDefault(x => x.userName == lg.userName && x.password == lg.password && x.isApproved == true);
+                if (log != null)
+                {
+                    return RedirectToAction("index");
+                }
+                else {
+                    ViewBag.msg = "invalid userName or password or not approved by Librarian!!";
+                }
+            }
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
---
+++ Library_mgt_system/Models/validation classes/tblloginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Library_mgt_system.Models.validation_classes
+{
+    public class tblloginModel
+    {
+        public int login_id { get; set; }
+        [Required]
+        public string userName { get; set; }
+        [Required]
+        public string password { get; set; }
+        public Nullable<bool> isApproved { get; set; }
+    }
+}
